781e230146bd3da66eb040ba8e058e76632de8e6
Oliver <oliver.ruggli@students.fhnw.ch>
2019-03-25 12:21:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php header( 'Location: /index.html' ) ;  ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>New Page</title>
+</head>
+<body>
+    <h1> DigiBp Morcote - elegant design</h1>
+</body>
+</html>

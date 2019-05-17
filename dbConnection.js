4b63f98a3c181657c68e43b53ca214e3591d0650
Oliver <oliver.ruggli@students.fhnw.ch>
2019-05-17 09:37:20 +0200

    implemented server side database queries

---
+++ dbConnection.js
@@ -0,0 +1,38 @@
+    var Pool = require('pg').Pool;
+    var config = {
+        host: 'ec2-54-247-70-127.eu-west-1.compute.amazonaws.com',
+        user:'mtqjbarwkwzsld',
+        password:'2b51f440930a3711f9cabf9884d82feda3b70e9b35c9e9fb2d5f6f989fef445a',
+        database: 'dd786fen583a5s',
+        ssl: true
+    };
+
+    var pool = new Pool(config);
+
+    async function get_items(){
+        var response = await pool.query("select * from customer ORDER BY c_id desc");
+        console.log(response.rows);
+        return response.rows;
+    }
+
+    async function get_IDandIncrease(){
+        var highestCustomerID = await pool.query({
+            rowMode: 'array',
+            text: "select c_id from customer ORDER BY c_id desc"
+        });
+        var newID =parseInt(highestCustomerID.rows[0])+ 1;
+        console.log(newID);
+        return newID;
+    }
+
+/*    async function addCustomertoDB(fname, lname, email, street, housenumber, postcode, city){
+        pool.query
+    }
+
+*/
+   /* async function addCustomer(varchar(45) cfn,var cln, var cemail, var cstreet, var chnumber, var cpostcode, var ccity ){
+        var response = await pool.query("select * from customer");
+    console.log(response.rows);
+    }*/
+
+//get_items() ;
